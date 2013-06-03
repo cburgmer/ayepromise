3a74d4d178f2b89b66b18d033a10c0eb6aed859e
Christoph Burgmer <cburgmer@ira.uka.de>
2013-06-03 23:11:34 +0200

    Add a value to a promise

--- aye.js
+++ aye.js
@@ -2,15 +2,23 @@ window.aye = (function () {
     var aye = {};
 
     aye.defer = function () {
-        var pending = true;
+        var pending = true,
+            result = null;
 
         return {
-            resolve: function () {
+            resolve: function (value) {
                 pending = false;
+                result = value;
             },
             promise: {
                 isPending: function () {
                     return pending;
+                },
+                valueOf: function () {
+                    if (pending) {
+                        throw new Error("Promise hasn't been resolved yet");
+                    }
+                    return result;
                 }
             }
         };
--- test/ayeSpec.js
+++ test/ayeSpec.js
@@ -6,12 +6,26 @@ describe("aye", function () {
             expect(defer.promise.isPending()).toBe(true);
         });
 
-        it("should indicate a promise resolved promise", function () {
+        it("should indicate a resolved promise", function () {
             var defer = aye.defer();
 
             defer.resolve();
 
             expect(defer.promise.isPending()).toBe(false);
+        });
+
+        it("should return the result", function () {
+            var defer = aye.defer();
+
+            defer.resolve("21");
+
+            expect(defer.promise.valueOf()).toEqual("21");
+        });
+
+        it("should throw an error if the promise hasn't been resolved yet", function () {
+            var defer = aye.defer();
+
+            expect(function () { defer.promise.valueOf() }).toThrow(new Error("Promise hasn't been resolved yet"));
         });
     });
 });
