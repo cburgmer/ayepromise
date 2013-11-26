98cac4f0b2d19395b16eeceb52a81d133b03be82
Christoph Burgmer <cburgmer@ira.uka.de>
2013-11-26 15:48:26 +0100

    Revert back to apromise-test for spec 1.0 for now, to make sure we at least hit that

--- test/complianceTestSuiteAdapter.js
+++ test/complianceTestSuiteAdapter.js
@@ -1,10 +1,12 @@
 var ayepromise = require('../ayepromise.js');
 
-module.exports.deferred = function () {
-    var defer = ayepromise.defer();
-    return {
-        promise: defer.promise,
-        resolve: defer.resolve,
-        reject: defer.reject
-    };
+module.exports = {
+    pending: function () {
+        var defer = ayepromise.defer();
+        return {
+            promise: defer.promise,
+            fulfill: defer.resolve,
+            reject: defer.reject
+        };
+    }
 };
